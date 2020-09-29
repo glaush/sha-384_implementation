51e0b4fa304e5de2a36cbd5d87ea320703e90782
unknown <lessoctominds@gmail.com>
2020-09-29 05:52:48 +0300

    Added user interface and reading from file

--- sha-384_implementation.cpp
+++ sha-384_implementation.cpp
@@ -1,17 +1,42 @@
 ﻿#include <iostream>
+#include <fstream>
 #include "SHA384.h"
 
 int main()
 {
 				try
 				{
-				std::cout << "Enter the message: ";
-
+				std::cout << "Read message from file or stream? (f/s)\n";
+		
 				std::string message;
+				char your_choice;
 
-				getline(std::cin,message,'\n');
+				std::cin >> your_choice;
 
-				/*Message<std::string>::set_message("abc");*/
+				if (your_choice == 'f')
+				{
+								std::cout << "Please, enter the path to file: ";
+								std::string path_to_file;
+								std::cin >> path_to_file;
+								std::ifstream file(path_to_file);
+								
+								if (file.is_open())
+								{
+												file >> message;
+								}
+								else
+												throw std::runtime_error("Error with file!\n");
+				}
+				else
+				{
+								std::cout << "Enter the message: ";
+
+								std::cin.ignore(std::cin.rdbuf()->in_avail());
+								std::cin.clear();
+								getline(std::cin, message, '\n');
+
+								/*Message<std::string>::set_message("abc");*/
+				}
 
 				Message<std::string>::set_message(message);
 
